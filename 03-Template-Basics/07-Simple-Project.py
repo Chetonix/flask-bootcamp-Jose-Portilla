0929ff6f2b634e3151d8fd4df0bd6d4646ba5bec
Chetan Harjani <chetanharjani@Chetans-MacBook-Pro.local>
2022-12-22 17:41:27 +0530

    add solution to the template exercise in 03-Template-Basics folder

--- 03-Template-Basics/07-Simple-Project.py
+++ 03-Template-Basics/07-Simple-Project.py
@@ -1,9 +1,12 @@
 # Set up your imports and your flask app.
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     # This home page should have the form.
-    pass
+    return render_template("07-solution-index.html")
 
 
 # This page will be the page after the form
@@ -16,8 +19,15 @@ def report():
     # https://stackoverflow.com/questions/26515422/how-to-check-if-last-character-is-integer-in-raw-input
 
     # Return the information to the report page html.
-    pass
+    report = False
+    username = request.args.get('username')
+    lower = any(c.islower() for c in username)
+    upper = any(c.isupper() for c in username)
+    num_end = username[-1].isdigit() 
+    if lower and upper and num_end:
+        report = True
+    return render_template("07-solution-report.html", report=report, lower=lower, upper=upper, num_end=num_end )
 
 if __name__ == '__main__':
     # Fill this in!
-    pass
+    app.run(debug=True)
